4cf7f9236c63b0e055244940c5277761ad1eb349
hyunwlee-dev <hwlee3928@gmail.com>
2023-04-27 01:04:58 +0900

    :sparkles: FEATURE | ThemeButton component

---
+++ src/components/ThemeButton/index.tsx
@@ -0,0 +1,43 @@
+import { Theme } from "@/types/theme";
+import styles from "./style.module.scss";
+import { ButtonHTMLAttributes } from "react";
+import Image, { ImageProps } from "next/image";
+
+interface IProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  theme: Theme;
+}
+
+const getAriaLabel = (theme: Theme) => {
+  if (theme === Theme.LIGHT) return "Dark Theme";
+  return "Light Theme";
+};
+
+const ThemeButton: React.FC<IProps> = ({ theme, className, ...restProps }) => {
+  const combineClassName = `${styles.themeButton} ${className}`;
+  return (
+    <button
+      className={combineClassName}
+      aria-label={getAriaLabel(theme)}
+      {...restProps}
+    >
+      {theme === Theme.LIGHT && (
+        <Image
+          src="/icons/icon-sun.svg"
+          alt="light mode"
+          width={26}
+          height={26}
+        />
+      )}
+      {theme === Theme.DARK && (
+        <Image
+          src="/icons/icon-moon.svg"
+          alt="dark mode"
+          width={26}
+          height={26}
+        />
+      )}
+    </button>
+  );
+};
+
+export default ThemeButton;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
+import ThemeButton from "@/components/ThemeButton";
+import { Theme } from "@/types/theme";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
     <>
-      <main></main>
+      <main>
+        <ThemeButton theme={Theme.LIGHT} />
+        <ThemeButton theme={Theme.DARK} />
+      </main>
     </>
   );
 };
