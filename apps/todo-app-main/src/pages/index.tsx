9a0aaf40d452dfb499518fffa10fb6379a137bb2
hyunwlee-dev <hwlee3928@gmail.com>
2023-05-28 02:52:01 +0900

    chore: 의미없는 import 삭제

--- apps/todo-app-main/src/pages/index.tsx
+++ apps/todo-app-main/src/pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from 'next';
 import Header from '@/components/Header';
 import TodoContainer from '@/container/TodoContainer';
-import { Button } from '@front-end-mentor/ui';
 
 const Home: NextPage = () => {
   return (
