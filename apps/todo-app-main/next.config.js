1f9b8d453da94437abc133931b2632a18aa03210
hyunwlee-dev <hwlee3928@gmail.com>
2023-07-15 23:13:59 +0900

    setting: fix todo-app-main shell script, fix disDir: 'build', output: 'export', image: 'upoptimized: true' in next.config.js

--- apps/todo-app-main/next.config.js
+++ apps/todo-app-main/next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   // reactStrictMode: true,
   transpilePackages: ['@front-end-mentor/ui'],
+  distDir: 'build',
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
